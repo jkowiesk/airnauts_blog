b592fa95d1ba060d663d26d59c4949051a0e1157
jkowiesk <jkowiesk@gmail.com>
2023-06-29 15:51:52 +0200

    what now

--- src/components/atoms/ArCookieBtn/ArCookieBtn.tsx
+++ src/components/atoms/ArCookieBtn/ArCookieBtn.tsx
@@ -1,13 +1,9 @@
-'use client';
-
 import React from 'react';
-import { setUserCookie } from 'helpers/setUserCookie';
 
 export function ArCookieBtn() {
   return (
     <button
       type="submit"
-      onClick={() => setUserCookie()}
       className="py-2 px-4 rounded-xl bg-red-300"
     >
       Log In/Out
