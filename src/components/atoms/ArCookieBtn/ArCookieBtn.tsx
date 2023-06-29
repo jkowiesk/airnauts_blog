e848b292558543776de18c7cbbd2867c6cc862f7
jkowiesk <jkowiesk@gmail.com>
2023-06-29 16:32:39 +0200

    dynamic functions

--- src/components/atoms/ArCookieBtn/ArCookieBtn.tsx
+++ src/components/atoms/ArCookieBtn/ArCookieBtn.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { cookies } from 'next/headers';
 
 export function ArCookieBtn() {
+  const cookieStore = cookies();
   return (
     <form>
       <button
         type="submit"
         className="py-2 px-4 rounded-xl bg-red-200"
       >
-        Log In/Out
+        {cookieStore.get('loggedIn')?.value === 'true' ? 'Logout' : 'Login'}
       </button>
     </form>
   );
