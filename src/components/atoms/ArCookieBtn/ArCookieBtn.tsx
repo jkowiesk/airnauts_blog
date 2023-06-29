0463f350a6cd8e0260ad0787391f21ee1c99ea82
jkowiesk <jkowiesk@gmail.com>
2023-06-29 15:56:26 +0200

    what if client2

--- src/components/atoms/ArCookieBtn/ArCookieBtn.tsx
+++ src/components/atoms/ArCookieBtn/ArCookieBtn.tsx
@@ -6,6 +6,9 @@ export function ArCookieBtn() {
   return (
     <button
       type="submit"
+      onClick={() => {
+        console.log('xd');
+      }}
       className="py-2 px-4 rounded-xl bg-red-300"
     >
       Log In/Out
