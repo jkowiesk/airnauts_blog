6894633541b0b1cc89cd196defa7071f127acdb8
jkowiesk <jkowiesk@gmail.com>
2023-06-29 16:19:37 +0200

    client2

--- src/components/atoms/ArCookieBtn/ArCookieBtn.tsx
+++ src/components/atoms/ArCookieBtn/ArCookieBtn.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { setUserCookie } from 'helpers/setUserCookie';
 
 export function ArCookieBtn() {
   return (
-    // @ts-expect-error Server Actions
-    <form action={setUserCookie}>
+    <form>
       <button
         type="submit"
         className="py-2 px-4 rounded-xl bg-red-200"
