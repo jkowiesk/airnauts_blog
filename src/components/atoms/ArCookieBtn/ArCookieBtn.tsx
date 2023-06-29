721910718f4d2987307f53b420c0d533f6e05e90
jkowiesk <jkowiesk@gmail.com>
2023-06-29 16:43:40 +0200

    repaired bug

--- src/components/atoms/ArCookieBtn/ArCookieBtn.tsx
+++ src/components/atoms/ArCookieBtn/ArCookieBtn.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { setUserCookie } from 'helpers/setUserCookie';
+import { cookies } from 'next/headers';
 
 export function ArCookieBtn() {
+  const cookieStore = cookies();
+
   return (
     // @ts-expect-error Server Actions
     <form action={setUserCookie}>
       <button
         type="submit"
-        className="py-2 px-4 rounded-xl bg-red-300"
+        className="py-2 px-4 rounded-xl bg-red-200"
       >
-        Log In/Out
+        {cookieStore.get('loggedIn')?.value === 'true' ? 'Logout' : 'Login'}
       </button>
     </form>
   );
