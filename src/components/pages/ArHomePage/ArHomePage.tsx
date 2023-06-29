16ad6ddd735b40debc83a8c79c515fb9dd5c24a0
jkowiesk <jkowiesk@gmail.com>
2023-06-29 15:01:49 +0200

    fixed test

--- src/components/pages/ArHomePage/ArHomePage.tsx
+++ src/components/pages/ArHomePage/ArHomePage.tsx
@@ -1,14 +1,14 @@
+import React from 'react';
 import { Metadata } from 'next';
-/* import React from 'react';
-import Link from 'next/link'; */
+import Link from 'next/link';
 
 export const ArHomePageMetadata: Metadata = {
   title: 'Homepage',
 };
 
 export function ArHomePage() {
-  return {
-    /* <menu className="w-full grid grid-rows-3 grid-cols-1 place-items-center gap-12 md:grid-cols-2 md:grid-rows-2 lg:grid-cols-3 lg:grid-rows-1 h-full">
+  return (
+    <menu className="w-full grid grid-rows-3 grid-cols-1 place-items-center gap-12 md:grid-cols-2 md:grid-rows-2 lg:grid-cols-3 lg:grid-rows-1 h-full">
       <Link
         className="bg-gray-100 w-48 text-3xl h-64 rounded-xl grid place-items-center"
         href={`/blog/${1}`}
@@ -27,6 +27,6 @@ export function ArHomePage() {
       >
         3
       </Link>
-    </menu> */
-  };
+    </menu>
+  );
 }
